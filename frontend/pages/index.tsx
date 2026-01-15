ea109df9423cee6177fd38dc68ce4b0b2851dd0d
Artur Moura <artur.dornelles@gmail.com>
2026-01-15 20:46:00 -0300

    Refactoring using react-query, adding simple optimistic UI updates

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,59 +1,91 @@
-import { useEffect, useState } from "react";
-import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+import { useState } from "react";
+import { fetchTasks, createTask, updateTask, Task } from "../services/index";
+import {
+  QueryClientProvider,
+  QueryClient,
+  useQueryClient,
+  useQuery,
+  useMutation,
+} from "@tanstack/react-query";
 
 // Decided to break this up into smaller components for better readability and maintainability.
 function Tasks() {
-  const [tasks, setTasks] = useState([]);
+  const queryClient = useQueryClient();
+  const [newTask, setNewTask] = useState("");
 
+  // Add data to the database
+  const { isPending, mutate } = useMutation({
+    mutationFn: (title: string) => createTask(title),
+    onSettled: async () => {
+      // Invalidate and refetch, wait for it to complete
+      await queryClient.invalidateQueries({ queryKey: ["tasks"] });
+      setNewTask("");
+    },
+    onError: (error) => {
+      alert(`Error updating task: ${error}`);
+      setNewTask("");
+    },
+  });
+
+  // Update data in the database
+  const updateMutation = useMutation({
+    mutationFn: ({ id, completed }: { id: string; completed: boolean }) =>
+      updateTask(id, completed),
+    onSuccess: () => {
+      // Invalidate and refetch
+      queryClient.invalidateQueries({ queryKey: ["tasks"] });
+    },
+    onError: (error) => {
+      alert(`Error updating task: ${error}`);
+    },
+  });
+
+  // Get data from the database
   // Here there was a bug where useEffect was missing a dependency array, causing infinite re-renders.
-  useEffect(() => {
-    fetch("/api/graphql", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({
-        query: `{ tasks { id title completed } }`,
-      }),
-    })
-      .then((res) => res.json())
-      .then((data) => setTasks(data.data.tasks));
-  }, []);
+  // I could just add the empty array, but I decided to refactor to use React Query for better data fetching and caching.
+  const { isError, isLoading, data } = useQuery({
+    queryKey: ["tasks"],
+    queryFn: fetchTasks,
+  });
 
-  const addTask = async () => {
-    const title = prompt("Task?");
-    await fetch("/api/graphql", {
-      method: "POST",
-      body: JSON.stringify({
-        query: `mutation { createTask(title: "${title}") { id title } }`,
-      }),
-      headers: { "Content-Type": "application/json" },
-    });
-  };
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
 
-  const toggleTask = async (id: string) => {
-    await fetch("/api/graphql", {
-      method: "POST",
-      body: JSON.stringify({
-        query: `mutation { toggleTask(id: "${id}") { id completed } }`,
-      }),
-      headers: { "Content-Type": "application/json" },
-    });
-  };
+  if (isError) {
+    return <div>Error loading tasks.</div>;
+  }
+
+  const tasks: Task[] | undefined = data?.data?.tasks;
 
   return (
     <div>
       <h1>Tasks</h1>
       <p>Click to complete the task</p>
-      <button onClick={addTask}>Add</button>
+      <button
+        onClick={() => {
+          const newTitle = prompt("Please input the name of your task");
+          if (newTitle !== null) {
+            setNewTask(newTitle);
+            mutate(newTitle);
+          }
+        }}
+      >
+        Add
+      </button>
 
-      {tasks?.map((t: any) => (
+      {tasks?.map((t: Task) => (
         <div
           key={t.id}
-          onClick={() => toggleTask(t.id)}
+          onClick={() =>
+            updateMutation.mutate({ id: t.id, completed: !t.completed })
+          }
           style={{ cursor: "pointer" }}
         >
           {t.title} — {t.completed ? "Done" : "Pending"}
         </div>
       ))}
+      {isPending && <li style={{ opacity: 0.5 }}>{newTask} — Pending</li>}
     </div>
   );
 }
