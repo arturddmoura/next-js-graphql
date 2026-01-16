e01e20f5864c4cbfb34c16a678ce21ca29649016
Artur Moura <artur.dornelles@gmail.com>
2026-01-16 09:44:32 -0300

    adding jest tests

---
+++ frontend/jest.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  roots: ['<rootDir>'],
+  testMatch: ['**/__tests__/**/*.ts?(x)', '**/?(*.)+(spec|test).ts?(x)'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  collectCoverageFrom: [
+    'services/**/*.{ts,tsx}',
+    '!**/*.d.ts',
+    '!**/node_modules/**',
+    '!**/.next/**',
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+  },
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+};
+
---
+++ frontend/jest.setup.js
@@ -0,0 +1,6 @@
+// Add custom jest matchers from jest-dom
+// This setup file is configured in jest.config.js
+
+// Mock fetch globally for all tests
+global.fetch = jest.fn();
+
